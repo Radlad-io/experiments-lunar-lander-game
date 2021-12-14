24d19d9be1818f5b5d1562391c55b433ea979751
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-12-14 18:21:57 -0500

    Fixed: Vite dist directory path for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 module.exports = defineConfig({
   build: {
+    outDir: path.join(__dirname, "dist"),
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
