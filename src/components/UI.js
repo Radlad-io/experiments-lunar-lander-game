110df31c45d602fe64496f86e5cc7b0740a93f1a
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-01-25 15:25:00 -0500

    Fixed: Instruction overlay issue

--- src/components/UI.js
+++ src/components/UI.js
@@ -12,7 +12,9 @@ gsap.to(title, { delay: 4, duration: 1, opacity: 0 });
 gsap.to(instructions, { delay: 5, duration: 0.75, opacity: 1 });
 
 startBtn.addEventListener("click", () => {
-  gsap.to(instructions, { duration: 0.5, opacity: 0 });
+  gsap
+    .to(instructions, { duration: 0.5, opacity: 0 })
+    .then((instructions.style.display = "none"));
   gsap.to(hud, { delay: 5, duration: 1, opacity: 1 });
   gsap.to(bottomBar, { delay: 5, duration: 1, opacity: 1 });
   initialCameraFlyIn();
