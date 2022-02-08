d62b2ca73420b5b6e70325a0ca13059d67435f13
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-02-07 20:48:01 -0500

    Finished: tests/lander

--- src/tests/altitude/scripts.js
+++ src/tests/altitude/scripts.js
@@ -4,6 +4,8 @@ import * as CANNON from "cannon-es";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
+// import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+// import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import { Text } from "troika-three-text";
 
 const scene = new THREE.Scene();
@@ -18,65 +20,20 @@ const world = new CANNON.World({
   // Moon -1.62 m/s²
 });
 
-const landerRenderBody = new THREE.Group();
-const landerMaterial = new THREE.MeshNormalMaterial();
-
 const radius = 5; // m
-const landerBodyPhysics = new CANNON.Body({
+const sphereBody = new CANNON.Body({
   mass: 50, // kg
+  shape: new CANNON.Box(new CANNON.Vec3(2.5, 2.5, 2.5)),
   material: new CANNON.Material(),
   velocity: new CANNON.Vec3(0, -10, 0),
-  angularFactor: new CANNON.Vec3(0, 0, 1),
-  angularDamping: 0.25,
 });
-landerBodyPhysics.position.set(0, 75, 0);
+sphereBody.position.set(0, 75, 0);
 
-// TODO: Lander rotates from center and it should rotate form the sphere's center
-// Top Part
-landerBodyPhysics.addShape(new CANNON.Sphere(3.5), new CANNON.Vec3(0, 3.5, 0));
-// Middle part
-landerBodyPhysics.addShape(new CANNON.Box(new CANNON.Vec3(2.5, 1.5, 2.5)));
-// Feet
-landerBodyPhysics.addShape(new CANNON.Sphere(1), new CANNON.Vec3(-3, -2.5, -3));
-landerBodyPhysics.addShape(new CANNON.Sphere(1), new CANNON.Vec3(-3, -2.5, 3));
-landerBodyPhysics.addShape(new CANNON.Sphere(1), new CANNON.Vec3(3, -2.5, -3));
-landerBodyPhysics.addShape(new CANNON.Sphere(1), new CANNON.Vec3(3, -2.5, 3));
+world.addBody(sphereBody);
 
-world.addBody(landerBodyPhysics);
-
-const geometry = new THREE.BoxGeometry(5, 3, 5);
-const sphereMesh = new THREE.Mesh(geometry, landerMaterial);
-sphereMesh.castShadow = true;
-landerRenderBody.add(sphereMesh);
-
-const landerTopGeometry = new THREE.SphereGeometry(3.5, 16, 8);
-const landerTopMesh = new THREE.Mesh(landerTopGeometry, landerMaterial);
-landerTopMesh.castShadow = true;
-landerTopMesh.position.set(0, 3.5, 0);
-landerRenderBody.add(landerTopMesh);
-
-const landerFirstFootGeometry = new THREE.SphereGeometry(1, 8, 8);
-const landerFistFootMesh = new THREE.Mesh(landerFirstFootGeometry, landerMaterial);
-landerFistFootMesh.castShadow = true;
-landerFistFootMesh.position.set(-3, -2.5, -3);
-landerRenderBody.add(landerFistFootMesh);
-const landerSecondFootGeometry = new THREE.SphereGeometry(1, 8, 8);
-const landerSecondFootMesh = new THREE.Mesh(landerSecondFootGeometry, landerMaterial);
-landerSecondFootMesh.castShadow = true;
-landerSecondFootMesh.position.set(-3, -2.5, 3);
-landerRenderBody.add(landerSecondFootMesh);
-const landerThirdFootGeometry = new THREE.SphereGeometry(1, 8, 8);
-const landerThirdFootMesh = new THREE.Mesh(landerThirdFootGeometry, landerMaterial);
-landerThirdFootMesh.castShadow = true;
-landerThirdFootMesh.position.set(3, -2.5, -3);
-landerRenderBody.add(landerThirdFootMesh);
-const landerFourthFootGeometry = new THREE.SphereGeometry(1, 8, 8);
-const landerFourthFootMesh = new THREE.Mesh(landerFourthFootGeometry, landerMaterial);
-landerFourthFootMesh.castShadow = true;
-landerFourthFootMesh.position.set(3, -2.5,  3);
-landerRenderBody.add(landerFourthFootMesh);
-
-scene.add(landerRenderBody);
+sphereBody.addEventListener("collide", (event) => {
+  console.log(event.contact.getImpactVelocityAlongNormal());
+});
 
 const groundBody = new CANNON.Body({
   type: CANNON.Body.STATIC,
@@ -86,6 +43,12 @@ const groundBody = new CANNON.Body({
 10;
 groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0); // make it face up
 world.addBody(groundBody);
+
+const geometry = new THREE.BoxGeometry(5, 5, 5);
+const material = new THREE.MeshNormalMaterial();
+const sphereMesh = new THREE.Mesh(geometry, material);
+sphereMesh.castShadow = true;
+scene.add(sphereMesh);
 
 const padGeometry = new THREE.CylinderBufferGeometry(10, 10, 0.1, 20);
 const padMaterial = new THREE.MeshPhongMaterial({
@@ -112,23 +75,11 @@ groundMesh.rotation.set(-Math.PI / 2, 0, 0);
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const testGeometry = new THREE.BoxGeometry(5, 5, 5);
-const testMaterial = new THREE.MeshNormalMaterial();
-const testSphereMesh = new THREE.Mesh(testGeometry, testMaterial);
-testSphereMesh.position.set(0, -10, 0);
-scene.add(testSphereMesh);
-
-landerBodyPhysics.addEventListener("collide", (event) => {
-  console.log(event.contact.getImpactVelocityAlongNormal());
-});
-
-import font from "./IBMPlexMono-Bold.ttf";
-
 // Set properties to configure:
 const altitudeText = new Text();
 scene.add(altitudeText);
 altitudeText.text = `ALTITUDE ${75}m`;
-altitudeText.font = font;
+altitudeText.font = "./IBMPlexMono-Bold.ttf";
 altitudeText.fontSize = 6;
 altitudeText.position.y = 25;
 altitudeText.position.z = -10;
@@ -137,7 +88,7 @@ altitudeText.color = 0xffffff;
 
 const vertVelocityText = new Text();
 scene.add(vertVelocityText);
-vertVelocityText.font = font;
+vertVelocityText.font = "./IBMPlexMono-Bold.ttf";
 vertVelocityText.fontSize = 6;
 vertVelocityText.position.y = 18;
 vertVelocityText.position.z = -10;
@@ -146,7 +97,7 @@ vertVelocityText.color = 0xffffff;
 
 const horizVelocityText = new Text();
 scene.add(horizVelocityText);
-horizVelocityText.font = font;
+horizVelocityText.font = "./IBMPlexMono-Bold.ttf";
 horizVelocityText.fontSize = 6;
 horizVelocityText.position.y = 11;
 horizVelocityText.position.z = -10;
@@ -154,41 +105,39 @@ horizVelocityText.position.x = -25;
 horizVelocityText.color = 0xffffff;
 
 const altitudeRayCaster = new THREE.Raycaster();
-// const origin = landerBodyPhysics.position
-const rayTo = new THREE.Vector3(0, -1, 0);
-rayTo.normalize();
-altitudeRayCaster.set(landerBodyPhysics.position, rayTo);
-let intersects, altitude;
+// const origin = sphereBody.position
+const rayTo =  new THREE.Vector3(0, -1, 0)
+rayTo.normalize()
+altitudeRayCaster.set(sphereBody.position,rayTo);
+let intersects, altitude
 
 const updateAlititude = () => {
-  altitudeRayCaster.set(landerBodyPhysics.position, rayTo);
-  intersects = altitudeRayCaster.intersectObject(groundMesh);
-  altitude = Math.floor(intersects[0].distance - 1.5);
+  altitudeRayCaster.set(sphereBody.position,rayTo);
+  intersects = altitudeRayCaster.intersectObject(groundMesh)
+  altitude = Math.floor(intersects[0].distance -1.5)
   altitudeText.text = `ALTITUDE ${altitude}m`;
-};
+}
 
 const updateDroPosition = () => {
-  altitudeText.position.x = landerBodyPhysics.position.x - 25;
-  altitudeText.position.y = landerBodyPhysics.position.y + 25;
-  vertVelocityText.position.x = landerBodyPhysics.position.x - 25;
-  vertVelocityText.position.y = landerBodyPhysics.position.y + 18;
-  horizVelocityText.position.x = landerBodyPhysics.position.x - 25;
-  horizVelocityText.position.y = landerBodyPhysics.position.y + 11;
-};
+  altitudeText.position.x = sphereBody.position.x - 25;
+  altitudeText.position.y = sphereBody.position.y + 25;
+  vertVelocityText.position.x = sphereBody.position.x - 25;
+  vertVelocityText.position.y = sphereBody.position.y + 18;
+  horizVelocityText.position.x = sphereBody.position.x - 25;
+  horizVelocityText.position.y = sphereBody.position.y + 11;
+}
 
 const updateVelocity = () => {
-  vertVelocityText.text = `${
-    landerBodyPhysics.velocity.y.toFixed(1) > 0 ? "↑" : "↓"
-  } ${Math.abs(landerBodyPhysics.velocity.y.toFixed(1))}m/s`;
-  horizVelocityText.text = `${
-    landerBodyPhysics.velocity.x.toFixed(1) > 0 ? "→" : "←"
-  } ${Math.abs(landerBodyPhysics.velocity.x.toFixed(1))}m/s`;
-};
+  vertVelocityText.text = `${sphereBody.velocity.y.toFixed(1) > 0 ? "↑": "↓"} ${Math.abs(sphereBody.velocity.y.toFixed(1))}m/s`;
+  horizVelocityText.text = `${sphereBody.velocity.x.toFixed(1) > 0 ? "→" : "←"} ${Math.abs(sphereBody.velocity.x.toFixed(1))}m/s`;
+}
 
 setInterval(() => {
-  updateAlititude();
-  updateVelocity();
-}, 100);
+  updateAlititude()
+  updateVelocity()
+}, 100)
+
+
 
 // Update the rendering:
 let color, intensity, distance, angle, penumbra, decay;
@@ -270,7 +219,6 @@ const droRotationZ = document.querySelector(".rotationZ");
 let landerRotation = 0;
 let pressedKeys = [];
 
-// FIXME: Thruster gets stuck on
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowUp":
@@ -314,30 +262,30 @@ function animate() {
   // required if controls.enableDamping or controls.autoRotate are set to true
   controls.update();
 
-  // console.log(landerBodyPhysics.velocity.y);
+  // console.log(sphereBody.velocity.y);
 
   if (pressedKeys.includes("ArrowUp")) {
-    landerBodyPhysics.applyLocalImpulse(new CANNON.Vec3(0, 3.5, 0));
+    sphereBody.applyLocalImpulse(new CANNON.Vec3(0, 3.5, 0));
   }
 
   // TODO: I'm currently setting the quaternion rotations for steering
   //  A better method might be to apply rotational force
   if (pressedKeys.includes("ArrowLeft")) {
     landerRotation -= 0.075;
-    landerBodyPhysics.quaternion.setFromEuler(0, 0, -landerRotation / 3);
+    sphereBody.quaternion.setFromEuler(0, 0, -landerRotation / 3);
     droRotationZ.innerHTML = landerRotation.toFixed(2);
   }
 
   if (pressedKeys.includes("ArrowRight")) {
     landerRotation += 0.075;
-    landerBodyPhysics.quaternion.setFromEuler(0, 0, -landerRotation / 3);
+    sphereBody.quaternion.setFromEuler(0, 0, -landerRotation / 3);
     droRotationZ.innerHTML = landerRotation.toFixed(2);
   }
 
-  landerRenderBody.position.copy(landerBodyPhysics.position);
-  landerRenderBody.quaternion.copy(landerBodyPhysics.quaternion);
+  sphereMesh.position.copy(sphereBody.position);
+  sphereMesh.quaternion.copy(sphereBody.quaternion);
 
-  updateDroPosition();
+  updateDroPosition()
 
   // Run the simulation independently of framerate every 1 / 60 ms
   world.fixedStep();
